79e76d60a692c61ea63600dce961396b1f212e31
Paul Ancajima <paulancajima@yahoo.com>
2023-05-05 21:31:54 -0700

    first commit

---
+++ ISS-Location/ISS_LocationApp.swift
@@ -0,0 +1,20 @@
+//
+//  ISS_LocationApp.swift
+//  ISS-Location
+//
+//  Created by Paul Ancajima on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ISS_LocationApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
